554c69fd1f1cf6897aec09aa1355bd0a0fbe2533
Afonso Coutinho <116525378+afonsopc@users.noreply.github.com>
2024-11-08 14:16:12 +0000

    Init

---
+++ get_next_line.c
@@ -0,0 +1,122 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: afpachec <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/02 00:18:59 by afpachec          #+#    #+#             */
+/*   Updated: 2024/11/08 12:50:24 by afpachec         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+
+char	*ft_strdup(const char *s1)
+{
+	char	*dup;
+	int		i;
+
+	dup = malloc(ft_strlen(s1) + 1);
+	if (dup == NULL)
+		return (dup);
+	i = -1;
+	while (s1[++i])
+		dup[i] = s1[i];
+	dup[i] = '\0';
+	return (dup);
+}
+
+ssize_t	memchr_distance(const void *str, int c, size_t n)
+{
+	unsigned char	*ptr;
+	unsigned char	uc;
+
+	ptr = (unsigned char *)str;
+	uc = (unsigned char)c;
+	while (n--)
+	{
+		if (*ptr == uc)
+			return ((char *)ptr - (char *)str);
+		ptr++;
+	}
+	return (-1);
+}
+
+char	*read_line(char buff[BUFFER_SIZE + 1], char *str[3], ssize_t distance)
+{
+	str[2] = ft_substr(buff, 0, distance);
+	if (!str[2])
+	{
+		free(str[2]);
+		free(str[1]);
+		return (NULL);
+	}
+	str[0] = ft_strjoin(str[1], str[2]);
+	if (!str[0])
+	{
+		free(str[2]);
+		free(str[1]);
+		return (NULL);
+	}
+	free(str[2]);
+	free(str[1]);
+	ft_memmove(buff, &buff[distance + 1], BUFFER_SIZE - distance - 1);
+	ft_memset(&buff[BUFFER_SIZE - distance - 1], '\0', distance + 1);
+	str[1] = str[0];
+	str[0] = ft_strjoin(str[1], "\n");
+	free(str[1]);
+	if (!str[0])
+		return (NULL);
+	return (str[0]);
+}
+
+ssize_t	read_fd(int fd, char buff[BUFFER_SIZE + 1])
+{
+	ssize_t		read_bytes;
+
+	read_bytes = read(fd, buff, BUFFER_SIZE);
+	buff[read_bytes] = '\0';
+	if (read_bytes <= 0)
+		ft_memset(buff, '\0', BUFFER_SIZE);
+	return (read_bytes);
+}
+
+char	*get_next_line(int fd)
+{
+	static char	buff[BUFFER_SIZE + 1];
+	char		*str[3];
+	ssize_t		distance;
+	ssize_t		read_bytes;
+
+	buff[BUFFER_SIZE] = '\0';
+	str[0] = malloc(1);
+	read_bytes = 1;
+	while (str[0] && read_bytes > 0)
+	{
+		str[1] = str[0];
+		distance = memchr_distance(buff, '\n', BUFFER_SIZE);
+		if (distance >= 0 && read_line(buff, str, distance))
+			return (str[0]);
+		str[0] = ft_strjoin(str[1], buff);
+		free(str[1]);
+		if (!str[0])
+			return (NULL);
+		read_bytes = read_fd(fd, buff);
+	}
+	if (ft_strlen(str[0]))
+		return (str[0]);
+	free(str[0]);
+	return (NULL);
+}
+
+// int	main(void)
+// {
+// 	int		fd;
+// 	char	*line;
+
+// 	fd = open("test.txt", O_RDONLY);
+// 	line = get_next_line(fd);
+// 	printf("!%s!\n", line);
+// 	close(fd);
+// }
---
+++ get_next_line.h
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: afpachec <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/02 00:19:18 by afpachec          #+#    #+#             */
+/*   Updated: 2024/11/08 12:47:22 by afpachec         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <unistd.h>
+# include <stdlib.h>
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 4
+# endif
+
+char	*get_next_line(int fd);
+char	*ft_substr(char const *s, unsigned int start, size_t len);
+ssize_t	memchr_distance(const void *str, int c, size_t n);
+char	*ft_strjoin(char const *s1, char const *s2);
+size_t	ft_strlen(const char *s);
+char	*ft_strdup(const char *s1);
+void	*ft_memmove(void *dst, const void *src, size_t len);
+void	*ft_memset(void *b, int c, size_t len);
+
+#endif
